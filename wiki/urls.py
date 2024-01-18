6ec92f871a037e2a4405caf9a5e54bdf92bdd686
ainsanchez <aasanchez@uchicago.edu>
2024-01-18 14:05:23 -0500

    Entry Page

--- wiki/urls.py
+++ wiki/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("encyclopedia.urls")),
-    path("wiki", views.index, name="index")
+    #path("wiki", views.index, name="index"),
+    path("wiki/<str:name>", views.title, name="title")
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+from encyclopedia import util
+
 # Create your views here.
 
 #To render an entire HTML page
 def index(request):
     return render(request, "wiki/index.html")
+
+def title(request, name):
+    return render(request, "wiki/title.html", { 
+      "name": util.get_entry(name)
+    })
